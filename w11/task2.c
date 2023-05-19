589e4406768e8f1a0b5d5a1c9e27c0a785452f29
LiquidIce95 <davidsanchez062@gmail.com>
2023-05-19 14:09:23 +0200

    df

--- w11/task2.c
+++ w11/task2.c
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
-bool is_palin(char* word, int size){
+bool is_palin(char* word){
+    int size = strlen(word);
     int k = 0;
     while(k < size-k && word[k] == word[size-k]){
         k++;
@@ -20,17 +22,25 @@ bool is_palin(char* word, int size){
 
 }
 
+char* init_str(char word[]){
+    int len = strlen(word);
+
+    char* res = malloc(sizeof(char)*len);
+
+    for(int k = 0; k < len; k++){
+        res[k] = word[k];
+    }
+
+    return res;
+}
 
 int main(){
 
     //testing is palindrome
     printf("%s", "test 1: #####\n");
+    char* word = init_str("boo");
     bool expected = false;
-    bool result = 0;
-    char* word = malloc(sizeof(char)*4);
-
-    
-
+    bool result = is_palin(word);
 
     printf("%s","expected: "); printf("%d",expected); printf("%s","\n");
     printf("%s", "result: "); printf("%d",result); printf("%s","\n");
@@ -43,6 +53,23 @@ int main(){
         printf("\033[31m FAIL \033[0m\n");
     }
 
+    printf("%s", "test 2: #####\n");
+
+    free(word);
+    word = init_str("abaaba");
+    expected = true;
+    result = is_palin(word);
+
+    printf("%s","expected: "); printf("%d",expected); printf("%s","\n");
+    printf("%s", "result: "); printf("%d",result); printf("%s","\n");
+
+    
+    if(result == expected){
+        printf("\033[32m SUCCESS \033[0m\n");
+    }
+    else{
+        printf("\033[31m FAIL \033[0m\n");
+    }
 
     return 0;
 }
